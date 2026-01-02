7003960f3f0ba5407661d87e14aea446b8927eb1
F4KHR111 <fakhriwirdiyan05@gmail.com>
2026-01-02 21:33:26 +0700

    Menambahkan dependensi untuk HalamanEntry

--- app/src/main/java/com/example/questfirebase/view/HalamanEntry.kt
+++ app/src/main/java/com/example/questfirebase/view/HalamanEntry.kt
@@ -1,5 +1,37 @@
 package com.example.questfirebase.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.KeyboardType.Companion.Text
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.questfirebase.R
+import com.example.questfirebase.modeldata.DetailSiswa
+import com.example.questfirebase.modeldata.UIStateSiswa
+import com.example.questfirebase.view.route.DestinasiEntry
+import com.example.questfirebase.viewmodel.EntryViewModel
+import com.example.questfirebase.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.launch
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EntrySiswaScreen(
