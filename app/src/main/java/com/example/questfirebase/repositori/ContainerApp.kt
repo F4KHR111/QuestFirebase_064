002874590d3017b910e40c448b90d61f9b2c6399
F4KHR111 <fakhriwirdiyan05@gmail.com>
2026-01-02 20:17:34 +0700

    Membuat container app dan implementasinya

---
+++ app/src/main/java/com/example/questfirebase/repositori/ContainerApp.kt
@@ -0,0 +1,12 @@
+package com.example.questfirebase.repositori
+
+interface ContainerApp {
+    val repositorySiswa: RepositorySiswa
+}
+
+class DefaultContainerApp : ContainerApp {
+    override val repositorySiswa: RepositorySiswa by lazy {
+        FirebaseRepositorySiswa()
+    }
+}
+
