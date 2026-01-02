e09329e41c747811030211d41761942725eeb8f0
F4KHR111 <fakhriwirdiyan05@gmail.com>
2026-01-02 21:23:00 +0700

    Membuat PetaNavigasi dan HostNavigasi untuk aplikasi

---
+++ app/src/main/java/com/example/questfirebase/view/controlNavigasi/PetaNavigasi.kt
@@ -0,0 +1,23 @@
+package com.example.questfirebase.view.controlNavigasi
+
+@Composable
+fun DataSiswaApp(navController: NavHostController = rememberNavController(), modifier: Modifier = Modifier) {
+    HostNavigasi(navController = navController)
+}
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(navController = navController, startDestination = DestinasiHome.route, modifier = Modifier){
+        composable(DestinasiHome.route) {
+            HomeScreen(navigateToItemEntry = { navController.navigate(DestinasiEntry.route)},
+                navigateToItemUpdate = {
+                    navController.navigate("${DestinasiDetail.route}/${it}")})
+        }
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(navigateBack = { navController.navigate(DestinasiHome.route)})
+        }
+    }
+}
