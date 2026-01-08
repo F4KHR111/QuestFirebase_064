ee5be4bf14686a97b98f931b7c4a94e10e3ec361
F4KHR111 <fakhriwirdiyan05@gmail.com>
2026-01-08 19:19:36 +0700

    Mengimplementasikan fungsi hapusSatuSiswa

--- app/src/main/java/com/example/questfirebase/repositori/RepositorySiswa.kt
+++ app/src/main/java/com/example/questfirebase/repositori/RepositorySiswa.kt
@@ -74,5 +74,7 @@ override suspend fun editSatuSiswa(id: Long, siswa: Siswa) {
 }
 
 override suspend fun hapusSatuSiswa(id: Long) {
-
+    val docQuery = collection.whereEqualTo("id", id).get().await()
+    val docId = docQuery.documents.firstOrNull()?.id ?: return
+    collection.document(docId).delete().await()
 }
