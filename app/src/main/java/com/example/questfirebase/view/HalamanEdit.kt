bc377d4daf9c33666e5968abbcc6786340537b13
F4KHR111 <fakhriwirdiyan05@gmail.com>
2026-01-08 19:35:04 +0700

    Membuat file HalamanEdit.kt dan menambahkan EditSiswaScreen

---
+++ app/src/main/java/com/example/questfirebase/view/HalamanEdit.kt
@@ -0,0 +1,21 @@
+package com.example.questfirebase.view
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditSiswaScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    Scaffold(
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiEdit.titleRes),
+                canNavigateBack = true,
+                navigateUp = onNavigateUp
+            )
+        },
+        modifier = modifier
+    )
+}
