ea7813c21423c5db102cc34213751f8b42f23251
F4KHR111 <fakhriwirdiyan05@gmail.com>
2026-01-08 19:30:52 +0700

    Menambahkan fungsi `getSatuSiswa` pada `DetailViewModel`

--- app/src/main/java/com/example/questfirebase/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questfirebase/viewmodel/DetailViewModel.kt
@@ -20,4 +20,17 @@ class DetailViewModel(
     init {
         getSatuSiswa()
     }
+
+    fun getSatuSiswa() {
+        viewModelScope.launch {
+            statusUIDetail = StatusUIDetail.Loading
+            statusUIDetail = try {
+                StatusUIDetail.Success(satusiswa = repositorySiswa.getSatuSiswa(idSiswa))
+            } catch (e: IOException) {
+                StatusUIDetail.Error
+            } catch (e: Exception) {
+                StatusUIDetail.Error
+            }
+        }
+    }
 }
