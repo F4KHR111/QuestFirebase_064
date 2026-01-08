1c4a62b446ee5d08ffde9a62b686f0a71b416f9b
F4KHR111 <fakhriwirdiyan05@gmail.com>
2026-01-08 19:17:04 +0700

    Mengimplementasikan fungsi `editSatuSiswa`

--- app/src/main/java/com/example/questfirebase/repositori/RepositorySiswa.kt
+++ app/src/main/java/com/example/questfirebase/repositori/RepositorySiswa.kt
@@ -61,5 +61,14 @@ override suspend fun getSatuSiswa(id: Long): Siswa? {
 }
 
 override suspend fun editSatuSiswa(id: Long, siswa: Siswa) {
-
+    val docQuery = collection.whereEqualTo("id", id).get().await()
+    val docId = docQuery.documents.firstOrNull()?.id ?: return
+    collection.document(docId).set(
+        mapOf(
+            "id" to siswa.id,
+            "nama" to siswa.nama,
+            "alamat" to siswa.alamat,
+            "telpon" to siswa.telpon
+        )
+    ).await()
 }
